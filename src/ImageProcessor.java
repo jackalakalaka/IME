0be6186772917060b2648d3eebc80e8ec9a9c572
Jack Carson <jackalakalaka@gmail.com>
2021-10-30 11:57:31 -0400

    some main stuff

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -1,2 +1,11 @@
-public class ImageProcessor {
+import controller.ImageController;
+import controller.ImageControllerImpl;
+import view.ImageView;
+import view.ImageViewImpl;
+
+public final class ImageProcessor {
+  public static void main(String[] args) {
+    ImageView view = new ImageViewImpl();
+    ImageController imgCtrlr = new ImageControllerImpl(System.in, )
+  }
 }
