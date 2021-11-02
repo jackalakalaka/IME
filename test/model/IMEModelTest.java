2f598f385ebea6fed48ccd169ef0593f8d6fed6e
Jack Carson <jackalakalaka@gmail.com>
2021-11-02 13:57:21 -0400

    model test beginnings

--- test/model/IMEModelTest.java
+++ test/model/IMEModelTest.java
@@ -2,15 +2,40 @@ package model;
 
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
+import java.util.HashMap;
+
+import model.FuncObjs.CommandsIntensity;
+import model.FuncObjs.ICommands;
+
+import static org.junit.Assert.assertEquals;
 
 public class IMEModelTest {
-  Image koala1;
+  private static final HashMap<String, Image> mtImages = new HashMap<>();
+  private static final HashMap<String, ICommands> mtCommands = new HashMap<>();
+  private final IMEModel model1 = new IMEModelImpl();
 
+  /**
+   * Test an invalid null images argument to the 2-arg constructor.
+   */
+  @Test(expected = NullPointerException.class)
+  public void constructor2ArgNullImages() {
+    IMEModel model = new IMEModelImpl(null, mtCommands);
+  }
+
+  /**
+   * Test an invalid null commands argument to the 2-arg constructor.
+   */
+  @Test(expected = NullPointerException.class)
+  public void constructor2ArgNullCommands() {
+    IMEModel model = new IMEModelImpl(mtImages, null);
+  }
+
+  /**
+   * Test addImage, getImageFromModel methods.
+   */
   @Test
-  public void test() throws FileNotFoundException {
-    this.koala1 = new ImagePpm("koalaoriginal.ppm");
-    this.koala1.getPixelAt(0, 0);
+  public void getImageFromModel() {
+    assertEquals(new CommandsIntensity(), this.model1.getImageFromModel());
   }
 
 }
